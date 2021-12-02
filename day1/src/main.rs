974284ff0054dff13a6a02520d8c2a8e62cfd288
jinshang <jinshang@tencent.com>
2021-12-02 19:14:00 +0800

    day1part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,6 +4,9 @@ fn main() {
         .map(|line| line.parse().unwrap())
         .collect();
     let ans = arr
+        .windows(3)
+        .map(|window| window.iter().sum())
+        .collect::<Vec<usize>>()
         .windows(2)
         .filter(|&window| window[0] < window[1])
         .count();
