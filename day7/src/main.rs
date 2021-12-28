32a5088c91f660fd35144cc0f3f45e513a986ebd
jinshang <jinshang@tencent.com>
2021-12-28 16:02:53 +0800

    day7part2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,16 +1,23 @@
 #![feature(int_abs_diff)]
 fn main() {
-    let mut nums: Vec<u64> = include_str!("input.txt")
+    let nums: Vec<u64> = include_str!("input.txt")
         .trim()
         .split(",")
         .map(|str| str.parse().unwrap())
         .collect();
 
-    nums.sort();
-    let mid = nums[nums.len() / 2];
+    let min = nums.iter().min().unwrap().to_owned();
+    let max = nums.iter().max().unwrap().to_owned();
 
-    let ans = nums
-        .iter()
-        .fold(0, |acc, &num| acc + u64::abs_diff(num, mid));
+    let ans = (min..max)
+        .map(|mid| {
+            nums.iter().fold(0, |acc, &num| {
+                let diff = u64::abs_diff(num, mid);
+                acc + diff * (diff + 1) / 2
+            })
+        })
+        .min()
+        .unwrap();
+
     println!("ans: {}", ans);
 }
