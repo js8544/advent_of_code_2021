e7365f411f1f507673320d17e4337452404b8ed3
jinshang <jinshang@tencent.com>
2021-12-28 22:20:46 +0800

    day10part2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,9 +1,15 @@
+use core::panic;
+use itertools::Itertools;
+
 fn main() {
-    let ans: u64 = include_str!("input.txt")
+    let scores: Vec<u64> = include_str!("input.txt")
         .lines()
         .map(str::as_bytes)
         .map(compute_score)
-        .sum();
+        .filter(|&score| score > 0)
+        .sorted()
+        .collect();
+    let ans = scores[scores.len() / 2];
     println!("ans: {}", ans);
 }
 
@@ -14,19 +20,22 @@ fn compute_score(line: &[u8]) -> u64 {
             stack.push(c);
         } else {
             if !is_match(stack.last().unwrap(), c) {
-                return match &c {
-                    b')' => 3,
-                    b']' => 57,
-                    b'>' => 25137,
-                    b'}' => 1197,
-                    _ => panic!(),
-                };
+                return 0;
             } else {
                 stack.pop();
             }
         }
     }
-    0
+    stack.iter().rev().fold(0, |acc, &cur| {
+        return acc * 5
+            + match cur {
+                b'(' => 1,
+                b'[' => 2,
+                b'{' => 3,
+                b'<' => 4,
+                _ => panic!("unexpected char {}", cur),
+            };
+    })
 }
 
 fn is_open(c: &u8) -> bool {
